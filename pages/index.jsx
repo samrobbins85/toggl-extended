8653f12bff17e79001509e440454d69bb8621a6b
Sam Robbins <samrobbinsgb@gmail.com>
2021-07-08 20:35:45 +0100

    Start on styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,21 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import Head from "next/head";
 import useSWR from "swr";
-import humanizeDuration from "humanize-duration";
+// Looking to get rid of this once the temporal proposal is out
+import { Duration } from "luxon";
+import { ClockIcon } from "@heroicons/react/outline";
+import { useState } from "react";
 
-// function normalDateTime(date) {
-// 	const data = new Date(date);
-// 	return data.toLocaleString("en-GB", { timeZone: "UTC" });
-// }
-
-function normalDate(date) {
-	const data = new Date(date);
-	return data.toLocaleDateString("en-GB", { timeZone: "UTC" });
+function formatDuration(duration) {
+	const length = Duration.fromObject({ milliseconds: duration });
+	return length.toFormat("hh:mm:ss");
 }
 
-function isSameDay(date1, date2) {
-	const parsedDate1 = new Date(date1);
-	const parsedDate2 = new Date(date2);
-	return parsedDate1.toDateString() === parsedDate2.toDateString();
-}
-
-export default function IndexPage() {
+export default function IndexPage({ er }) {
 	const { data, error } = useSWR("/reports/api/v2/details");
-	console.log(data);
+	const [rate, setRate] = useState(0);
+	// console.log(data);
+	// console.log(er);
 	return (
 		<>
 			<Head>
@@ -36,33 +31,64 @@ export default function IndexPage() {
 				</h1>
 				{error && <h2>There was an error in fetching the data</h2>}
 				{data && (
-					<div className="flex justify-center pt-4">
-						<div className="grid gap-y-6">
-							{data.data.map((item) => (
-								<div
-									className="bg-gray-50 p-4 grid grid-cols-2"
-									key={item.id}
-								>
-									<div>
-										<h3 className="font-semibold">
-											{item.description}
-										</h3>
-										<p className="text-gray-700">
-											{isSameDay(item.start, item.end)
-												? normalDate(item.start)
-												: `${normalDate(item.start)} -
-												  ${normalDate(item.end)}`}
-										</p>
-									</div>
-									<div className="italic text-right">
-										{humanizeDuration(item.dur)}
-									</div>
-								</div>
-							))}
+					<div className="grid max-w-lg mx-auto pt-4 gap-y-6">
+						<div className="bg-green-100 rounded p-4 text-green-900">
+							<h2 className="text-xl text-center">
+								Total time worked
+							</h2>
+							<p className="text-2xl text-center flex items-center justify-center ">
+								<ClockIcon className="h-6 w-6 mr-2" />
+								<p>{formatDuration(data.total_grand)}</p>
+							</p>
 						</div>
+						<div className="bg-gray-100 p-4">
+							<h2 className="text-2xl font-semibold">
+								Hourly rate
+							</h2>
+							<p className="text-gray-700">
+								How much you make per hour, and the currency
+							</p>
+						</div>
+						<div>
+							Pay in Dollars:{" "}
+							{(
+								(data.total_grand * rate) /
+								(1000 * 60 * 60)
+							).toFixed(2)}
+						</div>
+						<h2>US -&gt; UK Exchange Rate</h2>
+						<p>
+							{er.filter((item) => item.source === "USD")[0].rate}
+						</p>
+						<h2>Pay in pounds</h2>
+						<p>
+							{(
+								(er.filter((item) => item.source === "USD")[0]
+									.rate *
+									(data.total_grand * rate)) /
+								(1000 * 60 * 60)
+							).toFixed(2)}
+						</p>
 					</div>
 				)}
 			</div>
 		</>
 	);
+}
+
+export async function getStaticProps() {
+	const er = await fetch(
+		"https://api.sandbox.transferwise.tech/v1/rates?target=GBP",
+		{
+			headers: {
+				Authorization: `Bearer ${process.env.TRANSFERWISE_KEY}`,
+			},
+		}
+	).then((res) => res.json());
+
+	return {
+		props: {
+			er,
+		}, // will be passed to the page component as props
+	};
 }
